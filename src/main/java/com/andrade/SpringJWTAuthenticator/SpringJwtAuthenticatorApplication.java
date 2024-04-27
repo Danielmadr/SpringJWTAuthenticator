0f8ba94a661b83b0a20e82a9564baa70b728da27
Daniel <daniel.m.adr@hotmail.com>
2024-04-27 20:28:33 -0300

    first config

---
+++ src/main/java/com/andrade/SpringJWTAuthenticator/SpringJwtAuthenticatorApplication.java
@@ -0,0 +1,13 @@
+package com.andrade.SpringJWTAuthenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtAuthenticatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtAuthenticatorApplication.class, args);
+	}
+
+}
